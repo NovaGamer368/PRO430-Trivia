7f1e1331b1a4043bea25e2531d9ccd447ff473b5
Brennan Phifer <bman5803@gmail.com>
2024-07-08 10:04:21 -0600

    Initial creation

---
+++ data/mongoDAL.js
@@ -0,0 +1,2 @@
+// mongoDAL is responsible to for all interactions with mongodb for the trivia game
+// Switching Database is completely optional. I would stick with SQL
---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Time 4 Trivia', user: req.session.user, isAdmin: req.cookies.isAdmin });
+});
+
+router.get('/leaderboard', function(req, res, next) {
+  // TODO: Get actual leader data from the database!
+
+  // static data
+  let leaders = [
+    {
+      name: 'Sue', score: 100
+    },
+    {
+      name: 'Don', score: 99
+    },
+    {
+      name: 'Ralph', score: 3
+    }
+  ];
+
+  res.render('leaderboard', { title: 'Time 4 Trivia', user: req.session.user, isAdmin: req.cookies.isAdmin, leaders: leaders });
+});
+
+module.exports = router;

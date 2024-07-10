527a3c70a4cb893779f3ff309fcc3c03726c65c2
Brennan Phifer <bman5803@gmail.com>
2024-07-10 10:15:13 -0600

    Finished the first bullet point.
    Users must be logged in to play the game.

--- routes/game.js
+++ routes/game.js
@@ -1,10 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/play', function(req, res, next) {
+router.get("/play", function (req, res, next) {
   // TODO: Implement Game
-  
-  res.render('play', {user: req.session.user, isAdmin: req.cookies.isAdmin});
+  try {
+    console.log("user: ", req.session);
+
+    // Checking if user is logged in
+    if (req.session.user !== undefined) {
+      res.render("play", {
+        user: req.session.user,
+        isAdmin: req.cookies.isAdmin,
+      });
+    } else {
+      // If user is not logged in, throw an error
+      throw new Error("User not logged in");
+    }
+  } catch (err) {
+    // Handle errors
+    console.error(err);
+    res.status(500).render("error", {
+      message: err.message,
+      error: err,
+    });
+  }
 });
 
 module.exports = router;

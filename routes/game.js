20ddc71f9001760545f21eeceed9230a5896491e
Brennan Phifer <bman5803@gmail.com>
2024-07-15 09:46:43 -0600

    Dynamic Shuffling of game

--- routes/game.js
+++ routes/game.js
@@ -3,12 +3,32 @@ const router = express.Router();
 
 const gameController = require("../controllers/gameController");
 
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 router.get("/play", async function (req, res, next) {
   // TODO: Implement Game
   try {
-    console.log("user: ", req.session);
+    // console.log("user: ", req.session);
     let questions = await gameController.getTriviaQuestions();
-    console.log("Router Questions:", await questions);
+    // console.log("Router Questions:", await questions);
+
+    questions.forEach((question) => {
+      const answers = [
+        { text: question.CorrectAnswer, isCorrect: true },
+        { text: question.WrongAnswer1, isCorrect: false },
+        { text: question.WrongAnswer2, isCorrect: false },
+        { text: question.WrongAnswer3, isCorrect: false },
+      ];
+
+      question.ShuffledAnswers = shuffle(answers);
+      console.log("Shuffle: ", answers);
+    });
 
     // Checking if user is logged in
     if (req.session.user !== undefined) {

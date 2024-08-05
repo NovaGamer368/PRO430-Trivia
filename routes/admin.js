e406ed2e6db32b712e0409897ad858bb90a6ae4f
Brennan Phifer <bman5803@gmail.com>
2024-08-05 13:37:27 -0600

    Removing the silly drop route

--- routes/admin.js
+++ routes/admin.js
@@ -1,35 +1,40 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const userController = require('../controllers/userController');
+const userController = require("../controllers/userController");
 
 // All Admin Routes should only be accessble to logged in Admins!
 
-router.get('/users/:role', async function (req, res, next) {
+router.get("/users/:role", async function (req, res, next) {
   let role = req.params.role;
 
   if (!req.session.user || !req.cookies.isAdmin) {
-    res.redirect('/');
+    res.redirect("/");
   } else {
     let users = await userController.getUsers(role);
 
-    res.render('users', { title: 'Time 4 Trivia', user: req.session.user, isAdmin: req.cookies.isAdmin, users: users });
+    res.render("users", {
+      title: "Time 4 Trivia",
+      user: req.session.user,
+      isAdmin: req.cookies.isAdmin,
+      users: users,
+    });
   }
 });
 
-router.get('/delete/:userId', async function (req, res, next) {
+router.get("/delete/:userId", async function (req, res, next) {
   let userId = req.params.userId;
 
   await userController.deleteUserById(userId);
 
-  res.redirect('/');
+  res.redirect("/");
 });
 
-router.get('/drop', async function (req, res, next) {
-  console.log("dropping database!");
-  await userController.drop();
+// router.get('/drop', async function (req, res, next) {
+//   console.log("dropping database!");
+//   await userController.drop();
 
-  res.redirect('/');
-});
+//   res.redirect('/');
+// });
 
 module.exports = router;
